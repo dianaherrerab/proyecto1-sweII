3663896f279cccce821deb77731071a271649d10
dianaherrerab <54422584+dianaherrerab@users.noreply.github.com>
2025-09-13 12:43:20 -0500

    v 0.2 Mapa base

---
+++ docs/program.js
@@ -0,0 +1,6 @@
+var map = L.map('map').setView([4.628020469146535, -74.06588750401777], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
