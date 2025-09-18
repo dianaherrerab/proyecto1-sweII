d157314d7b4f203017e63af4a4689a628b276041
dianaherrerab <54422584+dianaherrerab@users.noreply.github.com>
2025-09-17 21:03:00 -0500

    Update program.js
    Para leer archivo geojson

--- docs/program.js
+++ docs/program.js
@@ -4,3 +4,22 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+// Cargar archivo GeoJSON 
+fetch('./map.geojson')
+  .then(response => response.json())
+  .then(data => {
+    L.geoJSON(data, {
+      style: {
+        color: "blue",
+        weight: 2,
+        fillColor: "lightblue",
+        fillOpacity: 0.5
+      },
+      onEachFeature: function (feature, layer) {
+        if (feature.properties && feature.properties.nombre) {
+          layer.bindPopup("<b>" + feature.properties.nombre + "</b>");
+        }
+      }
+    }).addTo(map);
+  });
